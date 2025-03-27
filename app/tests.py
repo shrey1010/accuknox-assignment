789d375a588cb90b21f8677b5c4c663d3aaa4073
Shrey Shukla <shreyshukla1010@gmail.com>
2025-03-27 17:59:11 +0530

    feat: testcases

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.db import transaction
+from .models import Book, Log
+import threading
+import time
 
-# Create your tests here.
+class SignalTests(TestCase):
+    def test_sync_execution(self):
+        start = time.time()
+        Book.objects.create(title="Test Book")
+        end = time.time()
+        print(f"Total Time: {end - start:.2f} seconds")
+
+    def test_same_thread(self):
+        print(f"[Caller] Thread ID: {threading.get_ident()}")
+        Book.objects.create(title="Thread Check")
+
+    def test_transaction_scope(self):
+        try:
+            with transaction.atomic():
+                Book.objects.create(title="Rollback Book")
+                raise Exception("Force rollback")
+        except Exception:
+            pass
+
+        logs = Log.objects.all()
+        print("Logs after rollback:", list(logs))
